077fc1f34c306986482c94cdceffd145f61c0b8b
Dany Valdez <99849047+MagnusTerra@users.noreply.github.com>
2023-07-08 17:18:28 +0000

    se soluciono el error de poder igresar decimales por binarios

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
                 tem = deci.split('.')
                 binary = dec_to_bin(int(tem[0]))
                 frac_binary = frac_to_bin(tem[1])
-                print(f'La parte fraccionaria del número es {binary}.{frac_binary}')
+                print(f'La parte fraccionaria del número es {binary}.{frac_binary}\n')
                 main()
             elif deci == 'exit':
                 print('Fin del programa')
@@ -21,9 +21,15 @@ def main():
                 main()
         elif op == '2':
             binary = input('Ingrese el numero: ')
-            temp = bin_to_dec(binary)
-            print(f'El numero es: {temp}')
-            main()
+            numeros = ['2', '3', '4', '5', '6', '7', '8','9']
+            if any(numero in binary for numero in numeros):
+                print("Ese no un numero binario \n ")
+                main()
+            else:
+                temp = bin_to_dec(binary)
+                print(f'El numero es: {temp}\n')
+                main()
+            
         else:
             print('Esa no es una opcion valida')
             main()
